b2a93e2ec94dbbafdfd0e69198980c2cca05952c
Henri Willman <60850956+willheto@users.noreply.github.com>
2020-11-29 01:35:53 +0200

    Remove console.logs used for earlier debugging, import champions.json

--- rombeFrontend/src/matches/MatchFolder.js
+++ rombeFrontend/src/matches/MatchFolder.js
@@ -4,150 +4,13 @@ import axios from 'axios'
 import './matchfolder.css'
 import './Sidepanel'
 import Sidepanel from './Sidepanel'
+import Champions from '../champions.json'
 
 
 const DATATYPE_SOLOQ = 1
 const DATATYPE_FLEXQ = 0
 
 const MatchFolder = (props) => {
-
-    const champions =
-    {
-        "1": "Annie",
-        "10": "Kayle",
-        "101": "Xerath",
-        "102": "Shyvana",
-        "103": "Ahri",
-        "104": "Graves",
-        "105": "Fizz",
-        "106": "Volibear",
-        "107": "Rengar",
-        "11": "Master Yi",
-        "110": "Varus",
-        "111": "Nautilus",
-        "112": "Viktor",
-        "113": "Sejuani",
-        "114": "Fiora",
-        "115": "Ziggs",
-        "117": "Lulu",
-        "119": "Draven",
-        "12": "Alistar",
-        "120": "Hecarim",
-        "121": "Kha'Zix",
-        "122": "Darius",
-        "126": "Jayce",
-        "127": "Lissandra",
-        "13": "Ryze",
-        "131": "Diana",
-        "133": "Quinn",
-        "134": "Syndra",
-        "136": "Aurelion Sol",
-        "14": "Sion",
-        "143": "Zyra",
-        "15": "Sivir",
-        "150": "Gnar",
-        "154": "Zac",
-        "157": "Yasuo",
-        "16": "Soraka",
-        "161": "Vel'Koz",
-        "163": "Taliyah",
-        "164": "Camille",
-        "17": "Teemo",
-        "18": "Tristana",
-        "19": "Warwick",
-        "2": "Olaf",
-        "20": "Nunu",
-        "201": "Braum",
-        "202": "Jhin",
-        "203": "Kindred",
-        "21": "Miss Fortune",
-        "22": "Ashe",
-        "222": "Jinx",
-        "223": "Tahm Kench",
-        "23": "Tryndamere",
-        "236": "Lucian",
-        "238": "Zed",
-        "24": "Jax",
-        "240": "Kled",
-        "245": "Ekko",
-        "25": "Morgana",
-        "254": "Vi",
-        "26": "Zilean",
-        "266": "Aatrox",
-        "267": "Nami",
-        "268": "Azir",
-        "27": "Singed",
-        "28": "Evelynn",
-        "29": "Twitch",
-        "3": "Galio",
-        "30": "Karthus",
-        "31": "Cho'Gath",
-        "32": "Amumu",
-        "33": "Rammus",
-        "34": "Anivia",
-        "35": "Shaco",
-        "36": "Dr. Mundo",
-        "37": "Sona",
-        "38": "Kassadin",
-        "39": "Irelia",
-        "4": "Twisted Fate",
-        "40": "Janna",
-        "41": "Gangplank",
-        "412": "Thresh",
-        "42": "Corki",
-        "420": "Illaoi",
-        "421": "Rek'Sai",
-        "427": "Ivern",
-        "429": "Kalista",
-        "43": "Karma",
-        "432": "Bard",
-        "44": "Taric",
-        "45": "Veigar",
-        "48": "Trundle",
-        "5": "Xin Zhao",
-        "50": "Swain",
-        "51": "Caitlyn",
-        "53": "Blitzcrank",
-        "54": "Malphite",
-        "55": "Katarina",
-        "56": "Nocturne",
-        "57": "Maokai",
-        "58": "Renekton",
-        "59": "Jarvan IV",
-        "6": "Urgot",
-        "60": "Elise",
-        "61": "Orianna",
-        "62": "Wukong",
-        "63": "Brand",
-        "64": "Lee Sin",
-        "67": "Vayne",
-        "68": "Rumble",
-        "69": "Cassiopeia",
-        "7": "LeBlanc",
-        "72": "Skarner",
-        "74": "Heimerdinger",
-        "75": "Nasus",
-        "76": "Nidalee",
-        "77": "Udyr",
-        "78": "Poppy",
-        "79": "Gragas",
-        "8": "Vladimir",
-        "80": "Pantheon",
-        "81": "Ezreal",
-        "82": "Mordekaiser",
-        "83": "Yorick",
-        "84": "Akali",
-        "85": "Kennen",
-        "86": "Garen",
-        "89": "Leona",
-        "9": "Fiddlesticks",
-        "90": "Malzahar",
-        "91": "Talon",
-        "92": "Riven",
-        "96": "Kog'Maw",
-        "98": "Shen",
-        "99": "Lux"
-    }
 
     const [user, setUser] = useState([])
     const [matches, setMatches] = useState([])
@@ -164,7 +27,6 @@ const MatchFolder = (props) => {
     const findUser = (event) => {
 
         event.preventDefault()
-        console.log(user)
         axios.get('http://localhost:3001/summoneridbyname', {
             headers: {
                 summonerName: user
@@ -207,7 +69,7 @@ const MatchFolder = (props) => {
                 })
 
             })
-            console.log(resp.data)
+
             axios.get('http://localhost:3001/api/matches', {
                 headers: {
                     summonerId: resp.data.accountId
@@ -216,7 +78,7 @@ const MatchFolder = (props) => {
 
                 setMatches(resp.data.matches)
                 let gameInfo = [{}, {}, {}, {}, {}]
-                console.log(gameInfo)
+
 
                 for (let index = 0; index < 5; index++) {
 
@@ -225,7 +87,7 @@ const MatchFolder = (props) => {
                             matchId: resp.data.matches[index].gameId
                         }
                     }).then(resp => {
-                        console.log(resp.data)
+  
                         switch (resp.data.queueId) {
                             case 440:
                             gameInfo[index].gameMode = "Ranked Flex"
@@ -252,7 +114,7 @@ const MatchFolder = (props) => {
                             if (name.player.summonerName.toLowerCase() == user.toLowerCase()) {
                                 resp.data.participants.map(partiC => {
                                     if (partiC.participantId == name.participantId) {
-                                        gameInfo[index].champion = champions[partiC.championId]
+                                        gameInfo[index].champion = Champions[partiC.championId]
                                         if (partiC.stats.win == true) {
                                             gameInfo[index].win = "Voitto"
                                         } else if (partiC.stats.win == false) {
@@ -267,7 +129,7 @@ const MatchFolder = (props) => {
                                 })
                             }
                         })
-                        console.log(gameInfo[index])
+
                         gameInfo[index].id = index;
                         gameInfo[index].duration = resp.data.gameDuration / 60
 
@@ -278,7 +140,7 @@ const MatchFolder = (props) => {
                 setMatchDetails([])
                 setMatchDetails(gameInfo)
                 setTimeout(function () { setUser(''); }, 1000);
-                console.log(matchDetails)
+
 
             })
         })
@@ -288,7 +150,6 @@ const changeUser = (event) => {
     event.preventDefault()
 
     setUser(event.target.value)
-    console.log(user)
 }
 
 return (
--- rombeFrontend/src/matches/Sidepanel.js
+++ rombeFrontend/src/matches/Sidepanel.js
@@ -1,15 +1,12 @@
 import React, { useState, useEffect } from 'react'
 import './sidepanel.css'
 import './MatchFolder'
-import Axios from 'axios'
 
 const Sidepanel = (props) => {
 
-    console.log(props)
- 
     const [division1, setDivision1] = useState(0)
     const [division2, setDivision2] = useState(0)
-    console.log(division1 === 0)
+
     if (division1 === 0) {
         switch (props.rank.div1) {
             case "II":
@@ -53,39 +50,32 @@ const Sidepanel = (props) => {
         let rankingSolo = 'default'
         let rankingFlex = 'default'
 
-        console.log(typeof(props.rank.div1))
-        if(typeof(props.rank.div1) !== 'undefined')
-        {
+        if (typeof (props.rank.div1) !== 'undefined') {
             rankingSolo = props.rank.tier1
             rankingSolo = rankingSolo + "_"
             rankingSolo = rankingSolo + division1
         }
-        console.log(rankingSolo)
 
-        console.log(typeof(props.rank.div2))
-        if(typeof(props.rank.div2) !== 'undefined') 
-        {
+        if (typeof (props.rank.div2) !== 'undefined') {
             rankingFlex = props.rank.tier2
             rankingFlex = rankingFlex + "_"
             rankingFlex = rankingFlex + division2
             console.log(division2)
         }
 
-        console.log(rankingFlex)
         return <div class="sidepanel">
             <h2>Solo/Duo Queue</h2>
             <img class="rankImg" src={`https://opgg-static.akamaized.net/images/medals/${rankingSolo}.png?image=q_auto:best&v=1`} />
-            <h1>{props.rank.tier1} {props.rank.div1} {props.rank.lp1} LP</h1>
+            <h1>{props.rank.tier1} {props.rank.div1} {props.rank.lp1} {props.rank.tier1 ? "LP" : "Unranked"}</h1>
             <hr></hr>
             <h2>Flex Queue</h2>
             <img class="rankImg" src={`https://opgg-static.akamaized.net/images/medals/${rankingFlex}.png?image=q_auto:best&v=1`} />
-            <h1>{props.rank.tier2} {props.rank.div2} {props.rank.lp2} LP</h1>
+            <h1>{props.rank.tier2} {props.rank.div2} {props.rank.lp2} {props.rank.tier2 ? "LP" : "Unranked"}</h1>
         </div>
     }
 
 
     return (
-
         returnIconOfDivision(props)
     )
 
